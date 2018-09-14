f190dc1f6338d3830338c9ce3c78e10422692946
d12frosted <boris@d12frosted.io>
2018-09-14 23:04:50 +0300

    add some simple tests

---
+++ Cask
@@ -0,0 +1,12 @@
+(source gnu)
+(source melpa)
+(source org)
+
+(package-file "flyspell-correct.el")
+
+(files "flyspell-correct.el")
+
+(development
+ (depends-on "el-mock")
+ (depends-on "ert-runner")
+ (depends-on "undercover"))
---
+++ test/test-helper.el
@@ -0,0 +1,114 @@
+(require 'cl)
+(require 'el-mock)
+(require 'undercover)
+(require 'subr-x)
+
+(setq user-home-directory (concat (getenv "HOME") "/"))
+
+(undercover "*.el")
+
+(add-to-list 'load-path ".")
+(load "flyspell-correct.el")
+
+(defmacro with-no-mistakes (&rest body)
+  `(with-flyspell-buffer
+    "|The licenses for most software and other practical works
+are designed to take away your freedom to share and change the
+works. By contrast, the GNU General Public License is intended to
+guarantee your freedom to share and change all versions of a
+program--to make sure it remains free software for all its users.
+We, the Free Software Foundation, use the GNU General Public
+License for most of our software; it applies also to any other
+work released this way by its authors. You can apply it to your
+programs, too." ,@body))
+
+(defmacro with-mistake-in-the-middle|cursor-beginning (&rest body)
+  `(with-flyspell-buffer
+    "The licenses for most software and other practical works are
+designed to take away your freedom to share and change the works.
+By contrast, the GNU General Public License is intended to
+guarantee your freedom to share and change all |versiuns of a
+program--to make sure it remains free software for all its users.
+We, the Free Software Foundation, use the GNU General Public
+License for most of our software; it applies also to any other
+work released this way by its authors. You can apply it to your
+programs, too." ,@body))
+
+(defmacro with-mistake-in-the-middle|cursor-before (&rest body)
+  `(with-flyspell-buffer
+    "The licenses for most software and other practical works are
+designed to take away your freedom to share and change the works.
+By contrast, the GNU General Public License |is intended to
+guarantee your freedom to share and change all versiuns of a
+program--to make sure it remains free software for all its users.
+We, the Free Software Foundation, use the GNU General Public
+License for most of our software; it applies also to any other
+work released this way by its authors. You can apply it to your
+programs, too." ,@body))
+
+(defmacro with-mistake-in-the-middle|cursor-inside (&rest body)
+  `(with-flyspell-buffer
+    "The licenses for most software and other practical works are
+designed to take away your freedom to share and change the works.
+By contrast, the GNU General Public License is intended to
+guarantee your freedom to share and change all ver|siuns of a
+program--to make sure it remains free software for all its users.
+We, the Free Software Foundation, use the GNU General Public
+License for most of our software; it applies also to any other
+work released this way by its authors. You can apply it to your
+programs, too." ,@body))
+
+(defmacro with-mistake-in-the-middle|cursor-end (&rest body)
+  `(with-flyspell-buffer
+    "The licenses for most software and other practical works are
+designed to take away your freedom to share and change the works.
+By contrast, the GNU General Public License is intended to
+guarantee your freedom to share and change all versiuns| of a
+program--to make sure it remains free software for all its users.
+We, the Free Software Foundation, use the GNU General Public
+License for most of our software; it applies also to any other
+work released this way by its authors. You can apply it to your
+programs, too." ,@body))
+
+(defmacro with-mistake-in-the-middle|cursor-after (&rest body)
+  `(with-flyspell-buffer
+    "The licenses for most software and other practical works are
+designed to take away your freedom to share and change the works.
+By contrast, the GNU General Public License is intended to
+guarantee your freedom to share and change all versiuns of a
+program--to make sure it remains free software for all| its
+users. We, the Free Software Foundation, use the GNU General
+Public License for most of our software; it applies also to any
+other work released this way by its authors. You can apply it to
+your programs, too." ,@body))
+
+(defmacro with-flyspell-buffer (text &rest body)
+  `(with-temp-buffer
+     (insert ,text)
+     (let ((flyspell-correct-interface #'correct)
+           (ispell-dictionary-alist
+            '((nil "[A-Za-z]" "[^A-Za-z]" "[']" nil ("-B") nil iso-8859-1)
+              ("english" "[A-Za-z]" "[^A-Za-z]" "[']" nil ("-B") nil iso-8859-1)))
+           (ispell-current-dictionary "english"))
+       (flyspell-buffer)
+       (sync-cursor)
+       (message "cursor location is %s" (point))
+       (with-mock
+         (mock (window-start) => (buffer-end 0))
+         (mock (window-end) => (buffer-end 1))
+         ,@body))))
+
+(defmacro ensure-no-corrections ()
+  `(not-called correct))
+
+(defmacro ensure-correction (from to)
+  `(mock (correct * ,from) => ,to :times 1))
+
+(defun sync-cursor ()
+  (interactive)
+  (beginning-of-buffer)
+  (let ((cursor "|"))
+    (search-forward cursor)
+    (delete-backward-char (length cursor))))
+
+(defun correct (word candidates))
