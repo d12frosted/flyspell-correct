61548083a1686bfcc3fb4271d5947547126aa098
d12frosted <boris@d12frosted.io>
2018-09-15 16:50:59 +0300

    add tests for flyspell-correct-next function

--- test/flyspell-correct-test.el
+++ test/flyspell-correct-test.el
@@ -39,3 +39,45 @@ cursor is after the word."
   (with-mistake-in-the-middle|cursor-after
    (ensure-correction "versiuns" "versions")
    (flyspell-correct-previous (point))))
+
+(ert-deftest flyspell-correct-next-no-mistakes-test ()
+  "Test that `flyspell-correct-next' doesn't correct any word
+in a buffer with no mistakes."
+  (with-no-mistakes
+   (ensure-no-corrections)
+   (flyspell-correct-next (point))))
+
+(ert-deftest flyspell-correct-next-cursor-beginning-test ()
+  "Test that `flyspell-correct-next' corrects a word when
+cursor is at the beginning of the word."
+  (with-mistake-in-the-middle|cursor-beginning
+   (ensure-correction "versiuns" "versions")
+   (flyspell-correct-next (point))))
+
+(ert-deftest flyspell-correct-next-cursor-before-test ()
+  "Test that `flyspell-correct-next' doesn't correct any word
+when cursor is before the word."
+  (with-mistake-in-the-middle|cursor-before
+   (ensure-correction "versiuns" "versions")
+   (flyspell-correct-next (point))))
+
+(ert-deftest flyspell-correct-next-cursor-inside-test ()
+  "Test that `flyspell-correct-next' corrects a word when
+cursor is inside of the word."
+  (with-mistake-in-the-middle|cursor-inside
+   (ensure-correction "versiuns" "versions")
+   (flyspell-correct-next (point))))
+
+(ert-deftest flyspell-correct-next-cursor-end-test ()
+  "Test that `flyspell-correct-next' corrects a word when
+cursor is at the end of the word."
+  (with-mistake-in-the-middle|cursor-end
+   (ensure-correction "versiuns" "versions")
+   (flyspell-correct-next (point))))
+
+(ert-deftest flyspell-correct-next-cursor-after-test ()
+  "Test that `flyspell-correct-next' corrects a word when
+cursor is after the word."
+  (with-mistake-in-the-middle|cursor-after
+   (ensure-no-corrections)
+   (flyspell-correct-next (point))))
